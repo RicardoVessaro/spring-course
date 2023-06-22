5cd266bd27582d61c7f0904dd19abf18ef3feaa2
RicardoVessaro <ricardolvessaro@gmail.com>
2023-06-21 21:26:11 -0300

    Section 2 - 52 Lazy Initialization - Coding - Part 2

--- springcoredemo/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
+++ springcoredemo/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -1,10 +1,16 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 // @Primary annotation choose which class of N autowiring results. Now TrackCoach is primary
 // @Primary
+/*
+    Beans annotated with @Lazy are not initialized, they will be initialized when they are used for dependency injection
+    (as a dependency) or when they are explicitly request (endpoint of a @RestController for example).
+ */
+@Lazy
 public class TrackCoach implements Coach {
 
     public TrackCoach() {
