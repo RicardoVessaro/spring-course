6a827b2e8036356a329a23da086af56c60f84ecb
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-01 12:01:05 -0300

    Section 3 - 77 Reading Objects with JPA - Coding

--- cruddemo-student-parent/cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ cruddemo-student-parent/cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -29,8 +29,33 @@ public class CruddemoApplication {
 
 //			createStudent(studentDAO);
 
-			createMultipleStudents(studentDAO);
+//			createMultipleStudents(studentDAO);
+
+			readStudent(studentDAO);
 		};
+	}
+
+	// Reading objects
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create a student object
+		System.out.println("Creating a new student object ...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+
+		// save the student
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		int theId = tempStudent.getId();
+		System.out.println("Saved student. Generated id: " + theId);
+
+		// retrieve student based on the id: primary key
+		System.out.println("Retrieving student with id: " + theId);
+		Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		System.out.println("Found the student: " + myStudent);
 	}
 
 	// How the application handle primary keys
