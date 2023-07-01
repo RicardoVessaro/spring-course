9c5e9f77579e755b9d36a5a988d07f073ca5e992
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-01 11:20:34 -0300

    Section 3 - 72 Saving a Java Object with JPA - Coding - Part 1

---
+++ cruddemo-student-parent/cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,35 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+    @Repository: Specialized annotation for repositories,
+    supports component scanning and translates JDBC exceptions.
+ */
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    @Autowired // optional because have only one constructor
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    /*
+        @Transactional: Handle the transactional management
+            while we are performing an update (persisting).
+     */
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
