9b4bc3f49d4795bddecaf33a2476c3e0b5d4b05d
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-05 20:42:26 -0300

    Section 4 - 102 Spring Boot REST Path Variables - Coding

--- rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +15,38 @@ import java.util.List;
 public class StudentRestController {
     // Remember, Spring REST and Jackson convert the POJOs and JSONs
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
+
+    /*
+        @PathVariable define the parameter as a path variable.
+
+        The name of the parameter is the same in the path (into {})
+     */
+    // define endpoint or "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
+    }
+
 
 }
