195fcf1e87fb01d9931beefa677fe1778836ccc9
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-05 21:12:27 -0300

    Section 4 - 109 Spring Boot REST Exception Handling - Coding - Part 4

--- rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -75,4 +75,19 @@ public class StudentRestController {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
+    // Add another exception handler ... to catch any exception (catch all)
+    // Exception: To handle generic exceptions
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
