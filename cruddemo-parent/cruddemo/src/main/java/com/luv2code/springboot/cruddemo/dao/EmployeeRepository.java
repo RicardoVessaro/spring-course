271335fb16482c2db00d4ab1a867c901797409e6
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-10 19:39:33 -0300

    Section 4 - 132 Spring Boot REST: Spring Data JPA - Coding - Part 2

--- cruddemo-parent/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo-parent/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.cruddemo.dao;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// Spring JpaRepository is a boiler plate of the DAO, it gives us the common CRUD methods.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it ... no need to write any code LOL!
