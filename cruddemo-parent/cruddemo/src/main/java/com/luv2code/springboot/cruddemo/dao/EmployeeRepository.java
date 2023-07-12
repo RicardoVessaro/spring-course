88affd91df50202915f95edae855256b6eb62631
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-12 19:27:22 -0300

    Section 4 - 135 Spring Boot REST: Spring Data REST - Coding - Part 2

--- cruddemo-parent/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo-parent/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,12 @@ package com.luv2code.springboot.cruddemo.dao;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+    @RepositoryRestResource: Defines the path to the REST endpoint.
+        Useful for custom paths or irregular plurals.
+    In this example the custom path is "members".
+ */
+//@RepositoryRestResource(path="members")
 // Spring JpaRepository is a boiler plate of the DAO, it gives us the common CRUD methods.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
