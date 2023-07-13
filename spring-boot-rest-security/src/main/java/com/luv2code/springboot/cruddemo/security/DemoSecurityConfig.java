82efe815124c745498f76e2cd269cd9e12443573
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-12 21:14:29 -0300

    Section 5 - 144 Spring Boot REST API Security - Restrict URLs based on Roles - Coding - Part 1

---
+++ spring-boot-rest-security/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// Spring Security Configuration
+@Configuration
+public class DemoSecurityConfig {
+
+    // In memory configuration
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+            .username("john")
+            // noop is the encoding algorithm ID for plain text (no operation)
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build();
+
+        UserDetails mary = User.builder()
+            .username("mary")
+            // noop is the encoding algorithm ID for plain text (no operation)
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER")
+            .build();
+
+        UserDetails susan = User.builder()
+            .username("susan")
+            // noop is the encoding algorithm ID for plain text (no operation)
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+            .build();
+
+        // Since we defined our users here... Spring Boot will NOT use the users/pass from the application.properties file
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer -> {
+            configurer
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                // '**' is a wildcard to accept subpaths
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN");
+        });
+
+        // Use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+
+}
