145ca894cf557786c028532f90171a18b1c46223
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-21 22:34:40 -0300

    243 - @OneToMany - Coding - Annotate Course entity

--- spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -1,6 +1,6 @@
 package com.luv2code.cruddemo.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity(name="course")
 public class Course {
@@ -15,10 +15,17 @@ public class Course {
 
     // annotate fields
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title")
     private String title;
 
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+                            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
     public Course() {
