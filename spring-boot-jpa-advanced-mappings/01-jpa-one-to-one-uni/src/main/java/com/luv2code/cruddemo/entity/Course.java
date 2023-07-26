2c02bad638b7efdf5362cfdd31d57c10a330b84a
RicardoVessaro <ricardolvessaro@gmail.com>
2023-07-26 19:24:13 -0300

    263 - @OneToMany - Uni-Directional - Coding - Refactor Course Entity

--- spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package com.luv2code.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="course")
 public class Course {
@@ -28,6 +31,14 @@ public class Course {
                             CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    /*
+        Mapping the relationship
+        @JoinColumn: Use the "course_id" column of the table "review" (Entity Review) to map
+     */
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -58,6 +69,25 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+
+    public void addReview(Review theReview) {
+
+        if(reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
     }
 
     @Override
