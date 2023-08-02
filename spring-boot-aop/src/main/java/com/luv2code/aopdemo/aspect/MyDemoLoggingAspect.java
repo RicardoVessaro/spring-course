e390b6bf2adb115ab44700010a216f4157d6f8cb
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-01 21:57:15 -0300

    326 - AOP - Pointcut Expressions - Coding - Part 1

--- spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -9,6 +10,14 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
     // this is where we add all of our related advices for logging
+
+    /*
+        Pointcut are used to reuse pointcut expressions
+
+        The method name is the pointcut expression. In this case 'forDaoPackage'.
+     */
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
 
     // let's start with a @Before advice
     /*
@@ -19,7 +28,10 @@ public class MyDemoLoggingAspect {
 
         It matches addAccount() method in any class
      */
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    /*
+        using forDaoPackage pointcut.
+     */
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on method");
