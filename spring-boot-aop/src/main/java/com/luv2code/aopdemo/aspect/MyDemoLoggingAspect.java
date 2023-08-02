192afe63dae7eec84487e47a517d2df7b552f6f1
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-01 21:21:29 -0300

    318 - AOP - Pointcut Expressions - Coding - Match any add* Method

--- spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -19,7 +19,7 @@ public class MyDemoLoggingAspect {
 
         It matches addAccount() method in any class
      */
-    @Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")
+    @Before("execution(public void add*())")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>> Executing @Before advice on method");
