4ab224035bd77dd7d23d61769f81ccff17cb734d
RicardoVessaro <ricardolvessaro@gmail.com>
2023-08-10 19:39:01 -0300

    353 - AOP: @Around Advice - Coding - Part 3

--- spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-boot-aop/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -18,21 +18,31 @@ import java.util.List;
 @Order(2)
 public class MyDemoLoggingAspect {
 
-    @Around("execution(* com.luv2code.aopdemo.dao.*.getFortune(..))")
-    public Object aroundGetForrtune(
+    @Around("execution(* com.luv2code.aopdemo.service.*.getFortune(..))")
+    public Object aroundGetFortune(
             ProceedingJoinPoint theProceedingJoinPoint) throws Throwable {
 
         // print out method we are advising on
+        String method = theProceedingJoinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @Around on method: " + method);
 
         // get begin timestamp
+        long begin = System.currentTimeMillis();
 
         // now, let's execute the method
+        /*
+            'proceed' calls the target method.
+         */
+        Object result = theProceedingJoinPoint.proceed();
 
         // get end timestamp
+        long end = System.currentTimeMillis();
 
         // compute duration and display it
+        long duration = end - begin;
+        System.out.println("\n=====> Duration: " + duration / 1000.0 + " seconds");
 
-        return null;
+        return result;
     }
 
     /*
